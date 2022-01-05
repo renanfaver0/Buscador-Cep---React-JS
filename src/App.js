1d0cb57ef36f655814674b9758ca236d00148812
renanfaver0 <89169958+renanfaver0@users.noreply.github.com>
2022-01-05 09:52:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
+// Importações serviços utilizados.
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './style.css';
 import api from './Services/api';
 
+// Função principal para trazer informações dos ceps digitados pelo usuário.
 function App() {
-  
+
   const [input, setInput] = useState('');
   const [cep, setCep] = useState({});
 
   async function manipularProcurar() {
-    if(input === '') {
+    if (input === '') {
       alert("Informe algum CEP!")
       return;
     }
-    
+
     try {
       const response = await api.get(`${input}/json`);
       console.log(response.data);
       setCep(response.data)
       setInput("");
-    
-    }catch {
+
+    } catch {
       alert("Cep inválido, verifique e digite novamente!");
       setInput("")
     }
-    
+
   }
 
   return (
@@ -32,29 +34,29 @@ function App() {
       <h1 className="titulo-principal">Buscar CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP aqui" 
+        <input type="text" placeholder="Digite seu CEP aqui"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
 
         <button className="botao-procurar" onClick={manipularProcurar}>
-          <FiSearch size={25} color="#FFF"/>
+          <FiSearch size={25} color="#FFF" />
         </button>
       </div>
-        
 
-        {Object.keys(cep).length > 0 && (
-             <main className='principal'>
-                <h2>CEP: {cep.cep}</h2>
-            
-                <span>{cep.logradouro}</span>
-                <span>Complemento: {cep.complemento}</span>
-                <span>Bairro: {cep.bairro}</span>
-                <span>Cidade: {cep.localidade} - {cep.uf}</span>
-            </main>
-        )}
-       
-      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='principal'>
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Cidade: {cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+
+    </div>
   );
 }
 
--- src/Services/api.js
+++ src/Services/api.js
@@ -1,3 +1,4 @@
+// Api utilizada para a busca dos ceps digitados pelo usuário.
 import axios from "axios";
 
 const api = axios.create({
